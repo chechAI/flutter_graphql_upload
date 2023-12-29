de4f62609212724b2f0052cf1edf55b10cfd94a1
chichAIbelu <etesfaye@hotmail.com>
2023-12-30 01:08:48 +0300

    chore: image selection widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,50 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import "package:http/http.dart" show MultipartFile;
+import "package:http_parser/http_parser.dart" show MediaType;
+import 'package:image_picker/image_picker.dart';
 
-void main() async{
+const createAssets = r'''
+      mutation CreateAssets($input: [CreateAssetInput!]!) {
+          createAssets(input: $input) {
+              ... on Asset {
+                  id
+                  createdAt
+                  updatedAt
+                  name
+                  fileSize
+                  mimeType
+                  type
+                  preview
+                  source
+                  width
+                  height
+                  focalPoint {
+                      x
+                      y
+                  }
+                  tags {
+                      id
+                      value
+
+                  }
+              }
+              ... on ErrorResult {
+                  message
+              }
+          }
+      }
+  ''';
+
+const uploadImage = r"""
+    mutation($file: Upload!) {
+      upload(file: $file)
+    }
+""";
+
+void main() async {
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
@@ -10,12 +52,8 @@ void main() async{
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     final httpLink = HttpLink(
       dotenv.env['ADMIN_API']!,
     );
@@ -30,118 +68,144 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: GraphQLProvider(
         client: client,
         child: const Scaffold(
-          body: MyHomePage(title: 'Flutter Demo Home Page'),
-          ),
+          body: MyHomePage(title: 'Graphql image upload'),
         ),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  File? _image;
+  bool _uploadInProgress = false;
 
-  void _incrementCounter() {
+  _uploadImage(BuildContext context) async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _uploadInProgress = true;
+    });
+
+    var byteData = _image!.readAsBytesSync();
+
+    var multipartFile = MultipartFile.fromBytes(
+      'photo',
+      byteData,
+      filename: '${DateTime.now().second}.jpg',
+      contentType: MediaType("image", "jpg"),
+    );
+
+    var files = [multipartFile];
+    final input = files.map((file) => {"file": file, "tags": 'zzz'}).toList();
+    print('input: $input');
+
+    var opts = MutationOptions(
+        fetchPolicy: FetchPolicy.noCache,
+        document: gql(createAssets),
+        variables: {
+          "input": input,
+        });
+
+    var client = GraphQLProvider.of(context).value;
+    var results = await client.mutate(opts);
+
+    setState(() {
+      _uploadInProgress = false;
+    });
+
+    var message = results.hasException
+        ? results.exception!.graphqlErrors.join(", ")
+        : "Image was uploaded successfully!";
+    print(message);
+    // final snackBar = SnackBar(content: Text(message));
+    // Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  final ImagePicker _picker = ImagePicker();
+
+  Future selectImage() async {
+    var image = await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = File(image!.path);
+      print('image: $_image');
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            if (_image != null)
+              Flexible(
+                flex: 9,
+                child: Image.file(_image!),
+              )
+            else
+              const Flexible(
+                flex: 9,
+                child: Center(
+                  child: Text("No Image Selected"),
+                ),
+              ),
+            Flexible(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  ElevatedButton(
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(Icons.photo_library),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text("Select File"),
+                      ],
+                    ),
+                    onPressed: () => selectImage(),
+                  ),
+                  // Mutation Widget Here
+                ],
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _isLoadingInProgress() {
+    return _uploadInProgress
+        ? const CircularProgressIndicator()
+        : const Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(Icons.file_upload),
+              SizedBox(
+                width: 5,
+              ),
+              Text("Upload File"),
+            ],
+          );
   }
 }
